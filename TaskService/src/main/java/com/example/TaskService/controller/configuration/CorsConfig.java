e26bece32a89ec959ad74890032c2567d1a66c9d
BBohdann <bbodnar2003@gmail.com>
2026-07-26 23:39:15 +0300

    Refactored Cors Configuration on both services

--- TaskService/src/main/java/com/example/TaskService/controller/configuration/CorsConfig.java
+++ TaskService/src/main/java/com/example/TaskService/controller/configuration/CorsConfig.java
@@ -1,15 +1,9 @@
 package com.example.TaskService.controller.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
 
 @Configuration
 public class CorsConfig {
@@ -19,28 +13,12 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                String[] allowedIps = generateAllowedIps(101, 110);
-                String[] allowedOriginPatterns = mergeAllowedOriginPatterns(allowedIps);
-
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(allowedOriginPatterns)
+                        .allowedOriginPatterns("https://*.netlify.app", "https://*.github.io")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization", "Content-Security-Policy")
                         .allowCredentials(true);
-            }
-
-            private String[] generateAllowedIps(int start, int end) {
-                return IntStream.rangeClosed(start, end)
-                        .mapToObj(i -> "http://192.168.0." + i + ":3000")
-                        .toArray(String[]::new);
-            }
-
-            private String[] mergeAllowedOriginPatterns(String[] dynamicIps) {
-                List<String> origins = new ArrayList<>(Arrays.asList(dynamicIps));
-                origins.add("https://*.netlify.app");
-                origins.add("https://*.github.io");
-                return origins.toArray(new String[0]);
             }
         };
     }
--- UserService/src/main/java/com/example/UserService/controller/configuration/CorsConfig.java
+++ UserService/src/main/java/com/example/UserService/controller/configuration/CorsConfig.java
@@ -1,44 +1,24 @@
 package com.example.UserService.controller.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                String[] allowedIps = generateAllowedIps(101, 110);
-                String[] allowedOriginPatterns = mergeAllowedOriginPatterns(allowedIps);
-
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(allowedOriginPatterns)
+                        .allowedOriginPatterns("https://*.netlify.app", "https://*.github.io")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
-                        .exposedHeaders("Authorization")
+                        .exposedHeaders("Authorization", "Content-Security-Policy")
                         .allowCredentials(true);
-            }
-
-            private String[] generateAllowedIps(int start, int end) {
-                return IntStream.rangeClosed(start, end)
-                        .mapToObj(i -> "http://192.168.0." + i + ":3000")
-                        .toArray(String[]::new);
-            }
-            private String[] mergeAllowedOriginPatterns(String[] dynamicIps) {
-                List<String> origins = new ArrayList<>(List.of(dynamicIps));
-                origins.add("https://*.netlify.app");
-                origins.add("https://*.github.io");
-
-                return origins.toArray(new String[0]);
             }
         };
     }
